98278bf2165890cb7c451622c65e161f64e192ef
Mohammad Hossein Behboudi <117489286+MHBehboudi@users.noreply.github.com>
2025-08-31 05:35:14 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+
+# Update these paths to match your system's file structure
+
+# Example Path: '/mobile-eeg-stimulus-presenter/Stimuli'
+STIMULI_FOLDER = 'PASTE_THE_FULL_PATH_TO_YOUR_STIMULI_FOLDER_HERE'
+
+# Example Path: '/mobile-eeg-stimulus-presenter/LENA_Modified.xlsx'
+STIMULI_INFO_FILE = 'PASTE_THE_FULL_PATH_TO_YOUR_EXCEL_FILE_HERE'
+
+# Example Path: '/mobile-eeg-stimulus-presenter/data'
+DATA_SAVE_FOLDER = 'PASTE_THE_FULL_PATH_TO_YOUR_DATA_FOLDER_HERE'
